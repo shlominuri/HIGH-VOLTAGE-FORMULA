7a29da12d1979775f766a425a75a889b9f8d1b63
Renan Amram <renilol2006@gmail.com>
2026-09-18 20:24:03 +0300

    added LPF to vcu control

--- VCU_Control_initialization.m
+++ VCU_Control_initialization.m
@@ -3,4 +3,7 @@
 VCU_torque_rise_rate = 50;   % [Nm/s]
 VCU_torque_fall_rate = -50;  % [Nm/s]
 
+VCU_torque_lpf_fc = 10;   % [Hz] TEMPORARY
+VCU_torque_lpf_tau = 1 / (2*pi*VCU_torque_lpf_fc);   % [sec]
+
 disp('DONE')
