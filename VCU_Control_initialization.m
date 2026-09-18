2fd4cb87c2f6b65f2f16495f0d3488f29739cba3
Renan Amram <renilol2006@gmail.com>
2026-09-19 01:06:19 +0300

    added power limiter to VCU

--- VCU_Control_initialization.m
+++ VCU_Control_initialization.m
@@ -1,9 +1,16 @@
 % VCU Control parameters
 
+% Rate limiter
 VCU_torque_rise_rate = 50;   % [Nm/s]
 VCU_torque_fall_rate = -50;  % [Nm/s]
 
+% Low-pass filter
 VCU_torque_lpf_fc = 10;   % [Hz] TEMPORARY
 VCU_torque_lpf_tau = 1 / (2*pi*VCU_torque_lpf_fc);   % [sec]
 
+% Power limiting
+VCU_max_power = 75e3;       % [W] TEMPORARY, note that 75K is the mechanical power and not the actual power outputted by the battery
+VCU_omega_floor = 1;        % [rad/s], prevents division by zero
+
+fprintf('VCU_torque_lpf_tau = %.6f s\n', VCU_torque_lpf_tau)
 disp('DONE')
